aee1ab1771bfb93a06b844ecebc1f6307a93273e
iJaygupta <jayguptazzz@gmail.com>
2020-08-19 23:06:43 +0530

    fix

--- services/Enquiry/enquiry.js
+++ services/Enquiry/enquiry.js
@@ -1,7 +1,7 @@
 import APIError from '../../lib/APIError';
 import { msg } from '../../lib/messages';
 import ContactUs from '../../models/ContactUs';
-import Team from '../../models/Team';
+import Team from '../../models/team';
 import Enquiry from '../../models/Enquiry';
 const HttpStatus = require('http-status-codes');
 

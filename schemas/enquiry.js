eae9ce3bb4c87bfd0a60403021153d462b9a3949
amitpanday <amitp381997@gmail.com>
2020-08-23 16:54:14 +0530

    subscribe api

--- schemas/enquiry.js
+++ schemas/enquiry.js
@@ -13,6 +13,7 @@ module.exports = {
       "message": { "type": ["string"] },
       "name": { "type": ["string"] },
       "subject": { "type": ["string"] },
+      "slug": { "type": ["string"] },
     },
     "additionalProperties": false,
   },

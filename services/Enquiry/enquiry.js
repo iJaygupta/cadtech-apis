38bd9b79cd15e44d7209d5673e8c09eb7d98478e
iJaygupta <jayguptazzz@gmail.com>
2020-10-01 16:56:09 +0530

    bulk upload api

--- services/Enquiry/enquiry.js
+++ services/Enquiry/enquiry.js
@@ -5,12 +5,12 @@ import StudentCertificates from '../../models/StudentCertificates';
 import Team from '../../models/Team';
 import Common from '../../models/Common';
 import Enquiry from '../../models/Enquiry';
-import Agent from '../../models/Agent';
 const HttpStatus = require('http-status-codes');
 const helpers = require('../../common/utils');
 const convertHtmlToPdf = require('../../lib/pdfConverter').convertHtmlToPdf;
 var rimraf = require('rimraf');
 var uploader = require("./../../lib/fileHandler");
+var XLSX = require('xlsx');
 
 
 class EnquiryService {
@@ -140,114 +140,41 @@ class EnquiryService {
     }
 
     async uploadCsv(request, response) {
-        await new Promise((resolve, reject) => {
-            uploader.uploadFilesLocal("data", 1, request, response, async function (err, data) {
-                if (err) {
-                    reject({ error: true, message: 'Something Went Wrong', status: HttpStatus.INTERNAL_SERVER_ERROR });
-                } else {
-                    try {
-                        if (request.files && request.files[0]) {
-                            var file = request.files[0];
-                            var workbook = XLSX.readFile(file.path);
-                            rimraf(file.destination, function () { });
-                            if (workbook && workbook.SheetNames && workbook.SheetNames[0] && workbook.Sheets[workbook.SheetNames[0]]) {
-                                var data = uploader.readBulkFile(workbook.Sheets[workbook.SheetNames[0]]);
-                                if (data.length) {
-                                    let agentData = await Agent.insertMany(data);
-                                    return agentData;
-                                } else {
-                                    throw new APIError({
-                                        message: 'You Are Trying To Upload Empty File. Please try again later', status: HttpStatus.UNPROCESSABLE_ENTITY
-                                    });
-                                }
+        uploader.uploadFilesLocal("user", "cetificate", 1, request, response, async function (err, data) {
+            if (err) {
+                throw new APIError({ message: 'Something Went Wrong', status: HttpStatus.INTERNAL_SERVER_ERROR });
+            } else {
+                try {
+                    if (request.files && request.files[0]) {
+                        var file = request.files[0];
+                        var workbook = XLSX.readFile(file.path);
+                        rimraf(file.destination, function () { });
+                        if (workbook && workbook.SheetNames && workbook.SheetNames[0] && workbook.Sheets[workbook.SheetNames[0]]) {
+                            var data = uploader.readBulkFile(workbook.Sheets[workbook.SheetNames[0]]);
+                            if (data.length) {
+                                let agentData = await StudentCertificates.insertMany(data);
+                                return agentData;
                             } else {
                                 throw new APIError({
-                                    message: 'Unable to process uploaded file. Please try again later', status: HttpStatus.UNPROCESSABLE_ENTITY
+                                    message: 'You Are Trying To Upload Empty File. Please try again later', status: HttpStatus.UNPROCESSABLE_ENTITY
                                 });
                             }
                         } else {
                             throw new APIError({
-                                message: 'Attached file not found, please try again', status: HttpStatus.UNPROCESSABLE_ENTITY
+                                message: 'Unable to process uploaded file. Please try again later', status: HttpStatus.UNPROCESSABLE_ENTITY
                             });
                         }
-                    } catch (error) {
-                        throw error;
+                    } else {
+                        throw new APIError({
+                            message: 'Attached file not found, please try again', status: HttpStatus.UNPROCESSABLE_ENTITY
+                        });
                     }
+                } catch (error) {
+                    throw error;
                 }
-            })
+            }
         })
     }
 }
-
-
-
-//      uploadFilesLocal("data", 1, request, response, function (error, data) {
-//     if (error) {
-//         response.status(500).send({
-//             "error": true,
-//             "code": 500,
-//             "msg": "Something went wrong",
-//             "data": error
-//         });
-//         // throw new APIError({ message: '', status: HttpStatus.UNPROCESSABLE_ENTITY });
-
-//     } else {
-//         try {
-//             if (request.files && request.files[0]) {
-//                 var file = request.files[0];
-//                 var workbook = XLSX.readFile(file.path);
-//                 rimraf(file.destination, function () { });
-//                 if (workbook && workbook.SheetNames && workbook.SheetNames[0] && workbook.Sheets[workbook.SheetNames[0]]) {
-//                     var data = readBulkFile(workbook.Sheets[workbook.SheetNames[0]]);
-//                     if (data.length) {
-//                         Agent.insertMany(data)
-//                             .then((data) => {
-//                                 response.status(201).send({
-//                                     "error": false,
-//                                     "code": 201,
-//                                     "msg": "File Uploaded Successfully",
-//                                 });
-//                             })
-//                             .catch((error) => {
-//                                 response.status(500).send({
-//                                     "error": true,
-//                                     "code": 500,
-//                                     "msg": "Something went wrong",
-//                                     "data": error
-//                                 });
-//                             })
-//                     } else {
-//                         response.status(400).send({
-//                             "error": true,
-//                             "code": 400,
-//                             'msg': `You Are Trying To Upload Empty File. Please try again later`
-//                         });
-//                     }
-//                 } else {
-//                     response.status(500).send({
-//                         "error": true,
-//                         "code": 500,
-//                         'msg': `Unable to process uploaded file. Please try again later`
-//                     });
-//                 }
-//             } else {
-//                 response.status(400).send({
-//                     "error": true,
-//                     "code": 400,
-//                     'msg': `Attached file not found, please try again`
-//                 });
-//             }
-//         } catch (error) {
-//             response.status(500).send({
-//                 "error": true,
-//                 "code": 500,
-//                 "msg": "Something went wrong",
-//             });
-//         }
-//     }
-// })
-
-
-
 
 export default new EnquiryService();

719fd6caad02bfdc4a5be3f94e72b3eb7ab74ee5
khemraj7 <kumarkhemraj614@gmail.com>
2020-11-01 13:24:03 +0530

    Guide

--- routes/guide.js
+++ routes/guide.js
@@ -3,7 +3,7 @@ import GuideService from '../services/Guide/guide';
 const { sendSuccess, sendError } = require('../lib/handleResponse');
 const HttpStatus = require('http-status-codes');
 const validator = require('../lib/validator');
-const schema = require("../schemas/profile")
+const schema = require("../schemas/guide")
 
 class GuideRoute {
     constructor(router) {
@@ -16,13 +16,52 @@ class GuideRoute {
             '/v1/guide',
             this.getAllGuide.bind(this)
         );
+        this.router.post(
+            '/v1/guide',
+            security.auth.bind(this),
+            validator.validateAjv(schema.addGuide),
+            security.checkUserScope.bind(this, security.scope.ADMIN),
+            this.addGuide.bind(this)
+        );
+        this.router.delete(
+            '/v1/guide/:id',
+            security.auth.bind(this),
+            security.checkUserScope.bind(this, security.scope.ADMIN),
+            this.deleteGuide.bind(this)
+        );
+        this.router.get(
+            '/v1/guide/:id',
+            this.getSingleGuide.bind(this)
+        );
     }
-
-  
     async getAllGuide(req, res, next) {
         try {
             const $response = await GuideService.getAllGuide(req.body);
             sendSuccess(res, HttpStatus.OK, 2048, $response);
+        } catch (error) {
+            sendError(res, error);
+        }
+    }
+    async addGuide(req, res, next) {
+        try {
+            const $response = await GuideService.addGuide(req.body);
+            sendSuccess(res, HttpStatus.OK, 2049, $response);
+        } catch (error) {
+            sendError(res, error);
+        }
+    }
+    async deleteGuide(req, res, next) {
+        try {
+            const $response = await GuideService.deleteGuide(req.params.id);
+            sendSuccess(res, HttpStatus.OK, 2050, $response);
+        } catch (error) {
+            sendError(res, error);
+        }
+    }
+    async getSingleGuide(req, res, next) {
+        try {
+            const $response = await GuideService.getSingleGuide(req.params.id);
+            sendSuccess(res, HttpStatus.OK, 2051, $response);
         } catch (error) {
             sendError(res, error);
         }
--- services/Guide/guide.js
+++ services/Guide/guide.js
@@ -1,4 +1,7 @@
+import APIError from '../../lib/APIError';
 import Guide from '../../models/Guide';
+const HttpStatus = require('http-status-codes');
+
 
 class GuideService {
 
@@ -6,6 +9,39 @@ class GuideService {
         try {
             let data = await Guide.find({});
             return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async addGuide(data) {
+        try {
+            let guide = new Guide({
+                ...data
+            });
+            guide = await guide.save();
+            return guide;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async deleteGuide(id) {
+        try {
+            let result = await Guide.findByIdAndDelete(id);
+            if (!result) {
+                throw new APIError({ message: 'Guide does not exists', status: HttpStatus.UNPROCESSABLE_ENTITY });
+            }
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getSingleGuide(id) {
+        try {
+            let result = await Guide.findById(id);
+            if (!result) {
+                throw new APIError({ message: 'Guide does not exists', status: HttpStatus.UNPROCESSABLE_ENTITY });
+            }
+            return result;
         } catch (error) {
             throw error;
         }
